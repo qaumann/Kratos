d4435197828f5bd31943e0c96e3e661cd5db08c7
Joaquín Irazabal <jirazabal@cimne.upc.edu>
2020-10-01 09:38:30 +0200

    Own changes

--- applications/FemToDemApplication/python_scripts/MainCouplingFemDem.py
+++ applications/FemToDemApplication/python_scripts/MainCouplingFemDem.py
@@ -52,7 +52,7 @@ class MainCoupledFemDem_Solution:
         else: # 3D
             self.number_of_nodes_element = 4
             self.FEM_Solution.main_model_part.ProcessInfo[KratosFemDem.ERASED_VOLUME] = 0.0 # Sand Production Calculations
-            
+
         self.FEM_Solution.Initialize()
         self.DEM_Solution.Initialize()
 
@@ -239,7 +239,7 @@ class MainCoupledFemDem_Solution:
 
         # Print required info
         self.PrintPlotsFiles()
-        
+
         # MODIFIED FOR THE REMESHING
         self.FEM_Solution.GraphicalOutputExecuteFinalizeSolutionStep()
 
@@ -257,7 +257,7 @@ class MainCoupledFemDem_Solution:
 
         if self.DoRemeshing:
              self.RemeshingProcessMMG.ExecuteFinalizeSolutionStep()
-        
+
         if not self.is_slave:
             self.PrintResults()
 
@@ -936,8 +936,8 @@ class MainCoupledFemDem_Solution:
                 max_id_properties = prop.Id
         props = KratosMultiphysics.Properties(max_id_properties + 1)
         self.created_props_id = max_id_properties + 1
-        props[KratosDEM.STATIC_FRICTION] =  -0.5773502691896257
-        props[KratosDEM.DYNAMIC_FRICTION] = -0.5773502691896257
+        props[KratosDEM.STATIC_FRICTION] =  0.0
+        props[KratosDEM.DYNAMIC_FRICTION] = 0.0
         props[KratosDEM.WALL_COHESION] = 0.0
         props[KratosDEM.COMPUTE_WEAR] = False
         props[KratosDEM.SEVERITY_OF_WEAR] = 0.001
