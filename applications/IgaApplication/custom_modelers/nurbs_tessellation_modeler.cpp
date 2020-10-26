ff6815320da136c34e750316790030a625ce4275
Dagmawi Bekel <dagmawi.bekel@tum.de>
2020-10-26 16:35:06 +0100

    create nurbs_tessellation_modeler

---
+++ applications/IgaApplication/custom_modelers/nurbs_tessellation_modeler.cpp
@@ -0,0 +1,38 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+
+
+// Project includes
+#include "nurbs_tessellation_modeler.h"
+
+
+namespace Kratos
+{
+    ///@name Stages
+    ///@{
+
+    void NurbsTessellationModeler::SetupModelPart()
+    {
+        KRATOS_ERROR_IF_NOT(mParameters.Has("cad_model_part_name")) << "Missing \"cad_model_part\" section" << std::endl;
+        ModelPart& cad_model_part = mpModel->GetModelPart(mParameters["cad_model_part_name"].GetString());
+
+        KRATOS_ERROR_IF_NOT(mParameters.Has("analysis_model_part_name")) << "Missing \"analysis_model_part_name\" section" << std::endl;
+        ModelPart& analysis_model_part = mpModel->GetModelPart(mParameters["analysis_model_part_name"].GetString());
+
+        const std::string& rDataFileName = mParameters.Has("physics_file_name")
+            ? mParameters["physics_file_name"].GetString()
+            : "physics.iga.json";
+
+        KRATOS_WATCH("HERE")
+    }
+
+    ///@}
+
+}
